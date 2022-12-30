a595fc90b3bc508aae4b579373ae9d1cdf34c816
Piyush121005 <102759887+Piyush121005@users.noreply.github.com>
2022-12-30 22:02:17 +0800

    Update linearBinarySearch.c

--- linearBinarySearch.c
+++ linearBinarySearch.c
@@ -1,110 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// void display(int arr[], int n)
-// {
-//     for (int i = 0; i < n; i++)
-//     {
-//         printf("%d  ", arr[i]);
-//     }
-//     printf("\n");
-// }
-// int linear(int arr[], int n, int ele)
-// {
-//     int flag = 0;
-//     for (int i = 0; i < n; i++)
-//     {
-//         if (arr[i] == ele)
-//         {
-//             flag = 1;
-//             return i;
-//         }
-//     }
-//     if (flag == 0)
-//     {
-//         return -1;
-//     }
-// }
-// int binary(int arr[], int n, int ele)
-// {
-//     int i = 0;
-//     int j = n - 1;
-//     while (i <= j)
-//     {
-//         int mid = (i + j) / 2;
-//         if (arr[mid] == ele)
-//         {
-//             return mid;
-//         }
-//         if (arr[mid] < ele)
-//         {
-//             i = mid + 1;
-//         }
-//         else
-//         {
-//             j = mid - 1;
-//         }
-//     }
-//     return -1;
-// }
-
-// int main()
-// {
-//     int ch, n, ele, index1, index2;
-//     printf("Enter the size of array: ");
-//     scanf("%d", &n);
-//     int arr[n];
-//     for (int i = 0; i < n; i++)
-//     {
-//         printf("Enter the elements of array at index %d: ", i + 1);
-//         scanf("%d", &arr[i]);
-//     }
-//     display(arr, n);
-//     printf("Enter the element you want to search: ");
-//     scanf("%d", &ele);
-//     printf("Enter the method of searching(1 for linear and 2 for binary): ");
-//     scanf("%d", &ch);
-//     switch (ch)
-//     {
-//     case 1:
-//         index1 = linear(arr, n, ele);
-//         if (index1 != -1)
-//         {
-//             printf("Your element is present at index: %d", index1 + 1);
-//         }
-//         else
-//         {
-//             printf("Element not found! ");
-//         }
-//         break;
-//     case 2:
-//         index2 = binary(arr, n, ele);
-//         if (index2 != -1)
-//         {
-//             printf("Your element is present at index:%d", index2 + 1);
-//         }
-//         else
-//         {
-//             printf("Element not found!");
-//         }
-//         break;
-//     }
-
-//     return 0;
-// }
-
+//creating user defined datatype
 struct array
 {
     int tSize;
     int uSize;
     int *arr;
 };
+
+//function for creating an array
 void create(struct array *a, int total, int used)
 {
     a->tSize = total;
     a->uSize = used;
     a->arr = (int *)malloc(total * sizeof(int));
 }
+
+//function for filling elements in the array
 void fill(struct array *a)
 {
     printf("Enter the values of the element in the array below: \n");
@@ -114,6 +27,8 @@ void fill(struct array *a)
         scanf("%d", &a->arr[i]);
     }
 }
+
+//function for linear searching in an array.
 int linear(struct array *a, int element)
 {
     for (int i = 0; i < a->uSize; i++)
@@ -126,6 +41,9 @@ int linear(struct array *a, int element)
     }
     return -1;
 }
+//Time complexity of linear search in an array is O(n)
+
+//function for binary searching in an array
 int binary(struct array *a, int element)
 {
     int i = 0;
@@ -149,6 +67,8 @@ int binary(struct array *a, int element)
         }
     }
 }
+//time complexity of binary searching in an array is O(log(n))
+
 
 int main()
 {
@@ -157,4 +77,5 @@ int main()
     fill(a);
     // linear(a, 7);
     binary(a, 18);
+    return 0;
 }
